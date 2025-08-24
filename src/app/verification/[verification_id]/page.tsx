2675e412c84f0eb2a60cde29f942b5e7bcc2d5e4
Guilherme Buys <131329633+BuysDev@users.noreply.github.com>
2025-08-23 21:07:07 -0300

    fix: removing all prisma occurrences from email verification

--- src/app/verification/[verification_id]/page.tsx
+++ src/app/verification/[verification_id]/page.tsx
@@ -1,40 +1,7 @@
 'use server'
-import { prisma } from "@/lib/prisma"
-import { redirect } from "next/navigation"
 
-export default async function EmailVerification({ params }: { params: Promise<{ verificationId: string }> }) {
-    try {
-        const emailVerificationId = (await params).verificationId;
-
-        const user = await prisma.user.findUnique({
-            where: {
-                verificationId: emailVerificationId
-            }
-        });
-
-        if (!user) {
-            return redirect('/verification/invalid-token');
-        }
-
-        if (user.isVerified) {
-            return redirect('/verification/already-verified');
-        }
-
-        await prisma.user.update({
-            where: {
-                id: user.id
-            },
-            data: {
-                isVerified: true,
-                verifiedAt: new Date(), // Registrar quando foi verificado
-                verificationId: null
-            }
-        });
-
-        return redirect('/verification/success');
-
-    } catch (error) {
-        console.error('Verification error:', error);
-        return redirect('/verification/error');
-    }
+export default async function EmailVerification() {
+    return (
+        <h1>Email verified</h1>
+    )
 }
